1ada7ac0e557da96505a204cf5add8c453cb427c
luoym <13575458746@163.com>
2017-12-28 16:47:16 +0800

    修改题目列表图片显示；获取智慧卡页面点击复制提示修改；

--- dist/static/script/util-e9b2d8c452.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author luoym
- * @email 13575458746@163.com
- * @descrip 
- * @version v1.0.0
- */
-window.CONFIG={api:"http://cm.yilesi.cn",bind_redirect_url:"http://cm.yilesi.cn/parent/bind",courseRecord_redirect_url:"http://cm.yilesi.cn/parent/courseRecord"};
---
+++ dist/static/script/util.js
@@ -0,0 +1,8 @@
+window.CONFIG = {
+    // 'api': 'http://video.yls.com/privateapi',
+    // 'bind_redirect_url': 'http://video.yls.com/parent/bind',
+    // 'courseRecord_redirect_url': 'http://video.yls.com/parent/courseRecord'
+    'api': 'http://cm.yilesi.cn',
+    'bind_redirect_url': 'http://cm.yilesi.cn/parent/bind',
+    'courseRecord_redirect_url': 'http://cm.yilesi.cn/parent/courseRecord'
+}
